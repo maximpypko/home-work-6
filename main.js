a9bb4ccba7a5d95436774922b0e22cec3a51a908
Maxim Pypko <max-rewq@rambler.ru>
2020-11-30 15:30:53 +0200

    fixed bugs

--- main.js
+++ main.js
@@ -1,70 +1,70 @@
-var cylinder = {
-    radius : 5,
-    height : 10,
-    volume : function() {
-        var getVolume = Math.PI * (this.radius**2) * this.height;
-        return (getVolume);
-    },
-    square : function() {
-        var getSquare = 2 * Math.PI * this.radius * (this.height + this.radius);
-        return(getSquare);
+// var cylinder = {
+//     radius : 5,
+//     height : 10,
+//     volume : function() {
+//         var getVolume = Math.PI * (this.radius**2) * this.height;
+//         return (getVolume);
+//     },
+//     square : function() {
+//         var getSquare = 2 * Math.PI * this.radius * (this.height + this.radius);
+//         return(getSquare);
         
-    },
-}
-console.log('Объём циллиндра ' + cylinder.volume());
-console.log('Площадь циллиндра ' + cylinder.square());
+//     },
+// }
+// console.log('Объём циллиндра ' + cylinder.volume());
+// console.log('Площадь циллиндра ' + cylinder.square());
 
 ///////////////////////////////////////////////
 
-function getResult (a, b, c) {
-    var result;
-    switch(c) {
-        case '+': 
-        result = a + b;
-        break;
-        case '-': 
-        result = a - b;
-        break;
-        case '*': 
-        result = a * b;
-        break;
-        case '/': 
-        result = a / b;
-        break;
-        case '**': 
-        result = a ** b;
-        break;
-        case '%': 
-        result = a % b;
-        break;
-        default :
-        result = 'Введите математический знак'
-    }
-    return result;
-}
-console.log(getResult(10, 3, '*'))
+// function getResult (a, b, c) {
+//     var result;
+//     switch(c) {
+//         case '+': 
+//         result = a + b;
+//         break;
+//         case '-': 
+//         result = a - b;
+//         break;
+//         case '*': 
+//         result = a * b;
+//         break;
+//         case '/': 
+//         result = a / b;
+//         break;
+//         case '**': 
+//         result = a ** b;
+//         break;
+//         case '%': 
+//         result = a % b;
+//         break;
+//         default :
+//         result = 'Введите математический знак'
+//     }
+//     return result;
+// }
+// console.log(getResult(10, 3, '*'))
 
 //////////////////////////////////////////////
 
-function isCharPresent(string, symbol) {
-    for (var i = 0; i <= string.length; i++) {
-        if (string.charAt(i) == symbol) {
-            var result = true;
-            return result;
-        } else {
-            result = false;
-        }
-    }
-    return result;
-}
-console.log(isCharPresent('Hello JS', 'l'));
+// function isCharPresent(string, symbol) {
+//     for (var i = 0; i <= string.length; i++) {
+//         if (string.charAt(i) == symbol) {
+//             var result = true;
+//             return result;
+//         } else {
+//             result = false;
+//         }
+//     }
+//     return result;
+// }
+// console.log(isCharPresent('Hello JS', 'l'));
 
 //////////////////////////////////////////////
 
 function charIndexOf(string, symbol) {
     for (var i = 0; i <= string.length; i++) {
         if (string.charAt(i) == symbol) {
-            var result = true;
+            var result = i;
             return result;
         } else {
             result = -1;
@@ -72,6 +72,6 @@ function charIndexOf(string, symbol) {
     }
     return result;  
 }
-console.log(charIndexOf('Hello JS', 'p'));
+console.log(charIndexOf('Hello JS', 'J'));
 
 
